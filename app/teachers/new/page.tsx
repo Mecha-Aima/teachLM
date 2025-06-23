b82d6bf174027afd014b7074b385621b8b0d27bb
mecha-aima <aimo.malik77@gmail.com>
2025-06-23 19:17:34 +0500

    Added page routes

---
+++ app/teachers/new/page.tsx
@@ -0,0 +1,9 @@
+const NewTeacher = () => {
+    return (
+        <div>
+            <h1>New Teacher</h1>
+        </div>
+    )
+}
+
+export default NewTeacher
---
+++ app/teachers/page.tsx
@@ -0,0 +1,9 @@
+const TeachersLibrary = () => {
+    return (
+        <div>
+            <h1>Teachers Library</h1>
+        </div>
+    )
+}
+
+export default TeachersLibrary
